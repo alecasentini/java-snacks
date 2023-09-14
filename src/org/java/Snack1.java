2b1e8094c4d16410b8a6a3c7045f2b5f569c751d
alecasentini <ale.casentini@gmail.com>
2023-09-14 11:41:59 +0200

    Snack 1 completato

--- src/org/java/Snack1.java
+++ src/org/java/Snack1.java
@@ -1,5 +1,21 @@
 package org.java;
 
-public class Snack1 {
+import java.util.Scanner;
 
+public class Snack1 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.print("Inserisci un numero: ");
+		
+		int val= sc.nextInt();
+		
+		if ((val % 2) == 0) {
+			System.out.println("Numero: " + val);
+		} else {
+			System.out.println("Numero successivo: " + (val + 1));
+		}
+			
+	}
 }
